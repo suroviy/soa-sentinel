e1318f20f78404af74472aa70f59333241568feb
Marcus Reinhardt <webstone@gmail.com>
2015-08-17 20:51:26 +0200

    assets

--- src/SleepingOwl/Admin/Templates/TemplateDefault.php
+++ src/SleepingOwl/Admin/Templates/TemplateDefault.php
@@ -9,19 +9,19 @@ class TemplateDefault implements TemplateInterface
 	function __construct()
 	{
 		AssetManager::addStyle('admin::default/css/font-awesome.min.css');
-		AssetManager::addStyle(asset('packages/sleeping-owl/admin-lte-template/bootstrap/css/bootstrap.min.css'));
-		AssetManager::addStyle(asset('packages/sleeping-owl/admin-lte-template/css/AdminLTE.min.css'));
-		AssetManager::addStyle(asset('packages/sleeping-owl/admin-lte-template/css/_all-skins.min.css'));
-		AssetManager::addStyle(asset('packages/sleeping-owl/admin-lte-template/css/custom.css'));
-		AssetManager::addStyle(asset('packages/sleeping-owl/admin-lte-template/css/dataTables.bootstrap.css'));
-		AssetManager::addStyle(asset('packages/sleeping-owl/admin-lte-template/plugins/iCheck/all.css'));
+		AssetManager::addStyle('admin::default/bootstrap/css/bootstrap.min.css');
+		AssetManager::addStyle('admin::default/css/AdminLTE.min.css');
+		AssetManager::addStyle('admin::default/css/_all-skins.min.css');
+		AssetManager::addStyle('admin::default/css/custom.css');
+		AssetManager::addStyle('admin::default/css/dataTables.bootstrap.css');
+		AssetManager::addStyle('admin::default/plugins/iCheck/all.css');
 
 		AssetManager::addScript(route('admin.lang'));
 		AssetManager::addScript('admin::default/js/jquery-1.11.0.js');
-		AssetManager::addScript('admin::default/js/bootstrap.min.js');
-		AssetManager::addScript(asset('packages/sleeping-owl/admin-lte-template/js/app.js'));
-		AssetManager::addScript(asset('packages/sleeping-owl/admin-lte-template/js/admin.js'));
-		AssetManager::addScript(asset('packages/sleeping-owl/admin-lte-template/plugins/iCheck/icheck.min.js'));
+		AssetManager::addScript('admin::default/bootstrap/js/bootstrap.min.js');
+		AssetManager::addScript('admin::default/js/app.js');
+		AssetManager::addScript('admin::default/js/admin.js');
+		AssetManager::addScript('admin::default/plugins/iCheck/icheck.min.js');
 	}
 
 	public function view($view)
