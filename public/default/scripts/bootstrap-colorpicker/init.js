457bafa29bba4e012ff985bab639d4c6852996e7
Marcus Reinhardt <webstone@gmail.com>
2015-10-15 18:24:53 +0200

    added assets for color picker

---
+++ public/default/scripts/bootstrap-colorpicker/init.js
@@ -0,0 +1,22 @@
+$(function(){
+  	$('.bsselect').each(function () {
+		var $this = $(this);
+		var nullable = $this.data('nullable');
+		var maxOptions = false;
+
+		var options = {};
+
+		if ( typeof($this.attr('multiple')) == "undefined" && typeof(nullable) != "undefined" ) {
+			$this.attr('multiple', '');
+			maxOptions = 1;
+		}
+
+		$this.colorpicker({
+			'title': window.admin.lang.select.placeholder,
+			'maxOptions': maxOptions,
+			'DEFAULTS': {
+				'noneSelectedText': window.admin.lang.select.nothing
+			}
+		});
+	});
+});
