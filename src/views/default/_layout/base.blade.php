7c0a6d695b182a11bafb156ce0732efd4a389158
Marcus Reinhardt <webstone@gmail.com>
2015-10-27 00:40:59 +0100

    added trans() in head

--- src/views/default/_layout/base.blade.php
+++ src/views/default/_layout/base.blade.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>{{{ $title }}}</title>
+		<title>{{{ trans($title) }}}</title>
 		<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 
