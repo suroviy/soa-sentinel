64bca40e3227536db29fbedc75dbcc6cfbd9998e
Marcus Reinhardt <webstone@gmail.com>
2015-12-15 19:19:54 +0100

    fixed typo for datetimepicker asset #126

--- src/SleepingOwl/Admin/ColumnFilters/Date.php
+++ src/SleepingOwl/Admin/ColumnFilters/Date.php
@@ -21,7 +21,7 @@ class Date extends Text
 		AssetManager::addStyle('admin::default/plugins/bootstrap-datetime-picker/css/bootstrap-datetimepicker-custom.min.css');
 
 		AssetManager::addScript('admin::default/plugins/bootstrap-datetime-picker/js/moment-with-locales.min.js');
-		AssetManager::addScript('admin::default/plugins/bootstrap-datetime-picker/js/datetime/s_bootstrap-datetimepicker.min.js');
+		AssetManager::addScript('admin::default/plugins/bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js');
 		AssetManager::addScript('admin::default/scripts/bootstrap-datetime-picker/init.js');
 	}
 
